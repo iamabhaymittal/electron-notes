d7cc29e066c9fb70a93d2073ca494dd0d12c5b08
Abhay Mittal <iamabhaymittal@gmail.com>
2025-08-12 17:51:40 +0530

    Save current changes before switching repository

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ export default function App() {
     toast.success("IPC Message Sent");
   };
   return (
-    <h1 className="flex flex-col min-h-screen items-center justify-center">
-      <div className="flex gap-2 ">
-        <Button onClick={ipc}>Send IPC</Button>
+    <h1 className="flex flex-col min-h-screen items-center justify-center ">
+      <div className="flex gap-2  ">
+        <Button onClick={ipc}>Send IPCs</Button>
       </div>
     </h1>
   );
